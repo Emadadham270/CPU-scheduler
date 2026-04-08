c24bf51ae2b5985450c5f2912f6ebfba094e854e
Mariam <mariam.abdelmotaleb05@eng-st.cu.edu.eg>
2026-04-09 01:37:55 +0200

    process_generator init

---
+++ process_generator/process_generator.h
@@ -0,0 +1,33 @@
+#ifndef PROCESS_QUEUE_H
+#define PROCESS_QUEUE_H
+
+typedef struct processData
+{
+    long mtype;
+    int id;
+    int arrival;
+    int runtime;
+    int priority;
+} processData;
+
+typedef struct Node
+{
+    processData data;
+    struct Node *next;
+} Node;
+
+typedef struct Queue
+{
+    Node *front;
+    Node *rear;
+    int size;
+} Queue;
+
+Queue *createQueue();
+int isEmpty(Queue *q);
+void enqueue(Queue *q, processData p);
+processData dequeue(Queue *q);
+processData peek(Queue *q);
+void freeQueue(Queue *q);
+
+#endif
