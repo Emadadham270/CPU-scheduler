ae25cb8b32ba5c5e968779bcdeee672c34f157cb
Mariam <mr7093248@gmail.com>
2026-05-02 00:32:42 +0300

    fix

--- MMU/mmu.h
+++ MMU/mmu.h
@@ -8,13 +8,7 @@
 
 #define MEM_SIZE 32
 #define PAGE_TABLE_SIZE 64
-
-
-#include "../data structs/structs.h"
-#include <sys/types.h>
-#define FRAMES_NUM 32
-extern  Frame frames[FRAMES_NUM];
-
+extern int block_end_time;
 
 Frame RAM[MEM_SIZE];
 
@@ -24,11 +18,8 @@ short check(PCB *pcb, int vpt_address);
 void define_page_table(int pid, int frame_index);
 int check_free_frame();
 void put_page_in_frame(int pid, int page_number, int frame_index);
-
-#endif
-int fault_handler();
-
-extern int block_end_time;
+void fault_handler();
 void swap(int id, int frameIndex, Frame arr[],int type,int page);
 VirtualAddress parse_virtual_address(char *bin_str);
+
 #endif
